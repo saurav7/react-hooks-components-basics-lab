4925fe4309b25349139ae54e1af3fd00d91bb414
Saurav <saurav.ronaldo7@gmail.com>
2023-09-25 12:01:22 +1000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,14 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About(){
+  return(
+    <div id="about">
+    <h2>About</h2>
+  </div>
+  )
+  
+}
 
 
 function App() {
@@ -24,7 +32,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
